b3f33e676aa3d570015e984b3767e35597e9ce05
Madhurima Jindamwar <madhurimajindamwar@gmail.com>
2026-05-28 16:54:22 +0530

    Simple calculator code

---
+++ intern.cpp/first_code.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "Enter first number ";
+    double a;
+    cin >> a;
+    cout << " Enter second Number ";
+    double b;
+    cin >> b;
+    cout << "Addition of a and b is :" << (a+b) << endl;
+    cout << "Subraction of a and b is :" << (a-b) << endl;
+    cout << "Multiplication of a and b is :" << (a*b) << endl;
+    cout << "Division of a and b is :" << (a/b) << endl;
+    //cout << "Modulous of a and b is :" << (a%b) << endl;
+
+}
